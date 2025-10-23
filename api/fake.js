d674d821d9e2760784e2b5ae2e8a46041a342b94
Fabydanto79 <132086483+Fabydanto79@users.noreply.github.com>
2025-10-23 15:57:47 +0200

    Delete api directory

--- api/fake.js
+++
@@ -1,7 +0,0 @@
-const response = await fetch('/api/chat', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ message: userMessage })
-});
-const data = await response.json();
-displayMessage('Jarvis', data.reply);
